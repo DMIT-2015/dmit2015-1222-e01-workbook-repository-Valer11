210cf9bcd2455f9f99a42f820c07460ecc00df25
Valerii Dombrovskyi <vdombrovskyi2@nait.ca>
2023-01-18 20:40:44 -0700

    Getter ans Setter from lombok example

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/Rectangle.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/Rectangle.java
@@ -1,24 +1,28 @@
 package dmit2015.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class Rectangle {
     private double length;
     private double width;
 
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
+//    public double getLength() {
+//        return length;
+//    }
+//
+//    public void setLength(double length) {
+//        this.length = length;
+//    }
+//
+//    public double getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(double width) {
+//        this.width = width;
+//    }
 
     public Rectangle(double width, double length) {
         setWidth(width);
