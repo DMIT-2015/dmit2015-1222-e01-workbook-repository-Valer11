8b50b83a8474c443f59b1f2a65d52e469352ad80
Valerii Dombrovskyi <vdombrovskyi2@nait.ca>
2023-01-27 19:37:48 -0700

    Streams Demo

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxManager.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxManager.java
@@ -4,14 +4,31 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class CanadianPersonalIncomeTaxManager {
+
+    public List<String> readAllLinesFromCsvFile(String filePathString) {
+        List<String> allLines = new ArrayList<>();
+
+        try {
+            Path csvPath = Path.of(Thread
+                    .currentThread()
+                    .getContextClassLoader()
+                    .getResource(filePathString)
+                    .toURI());
+            allLines = Files.readAllLines(csvPath);
+            // Skip the first line using Streams
+            allLines = allLines.stream().skip(1).toList();
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return allLines;
+    }
     public static void main(String[] args) {
         try {
-            // Use the following code to access a resource file located in the `resources` folder your project.
-            // Your code must handle a `URISyntaxException` thrown the get `getResource()` method.
             Path csvPath = Path.of(Thread.currentThread().getContextClassLoader().getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
             // Use the `Files.readAllLines()` method to read all lines from a file as a List.
             System.out.println("I am reading all the lines from the csv file into a list of String.");
@@ -20,24 +37,10 @@ public class CanadianPersonalIncomeTaxManager {
             allLines.forEach(System.out::println);
             System.out.println("\n\n");
 
-            // Use the `Files.readString()` method to reads all content from a file into a String,
-            System.out.println("I am reading all content from the csv file into a String.");
-            String fileContent = Files.readString(csvPath);
-            System.out.println("I am printing the String content.");
-            System.out.println(fileContent);
-            System.out.println("\n\n");
 
-            // Use the `Files.lines()` method to read all lines from a file as a Stream.
-            System.out.println("I am reading all lines from the csv file as a Stream.");
-            Stream linesStream = Files.lines(csvPath);
-            System.out.println("I am printing all lines from the Stream.");
-            linesStream.forEach(System.out::println);
-            System.out.println("\n\n");
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
 
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
